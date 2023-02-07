fd7e6c85fa7e9b9fd1c4a5006fb85a7fd3a113fc
Marcin Pankiewicz-Paczyński <marcinpaczynski@o2.pl>
2023-02-07 10:05:11 +0100

    Finish Handler Event

--- js/script.js
+++ js/script.js
@@ -1,39 +1,46 @@
 'use strict';
-
-/* document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-  }); */
-
-  const titleClickHandler = function(event){
+const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
   
-    /* [done] remove class 'active' from all article links */
+    /* [DONE] remove class 'active' from all article links */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
-      activeLink.classList.remove('active');
+    activeLink.classList.remove('active');
     }
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
 
-    console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
 
     /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .active');
-    
+
     for(let activeArticle of activeArticles){
-      activeArticle.classList.remove('active');
-    } 
+    activeArticle.classList.remove('active');
+    }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
 
-    /*find the correct article using the selectior (value of 'href' attribute) */
+    const articleAttribute = clickedElement.getAttribute('href');
+    console.log(articleAttribute);
 
-    /* add class 'active' to the correct article */ 
+    /* [DONE] find the correct article using the selectior (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleAttribute);
+    console.log(targetArticle);
+
+    /* [DONE] add class 'active' to the correct article */ 
+
+    targetArticle.classList.add('active');
   }
+
+    const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
   
   const links = document.querySelectorAll('.titles a');
   
