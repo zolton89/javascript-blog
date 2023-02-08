bbb74a875afdfa1ec6c35d75fea0154d24a79a3d
Marcin Pankiewicz-Paczyński <marcinpaczynski@o2.pl>
2023-02-08 12:11:17 +0100

    Fix bug to check

--- js/script.js
+++ js/script.js
@@ -38,13 +38,63 @@ const titleClickHandler = function(event){
     targetArticle.classList.add('active');
   }
 
-    const optArticleSelector = '.post',
-    optTitleSelector = '.post-title',
-    optTitleListSelector = '.titles';
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* [DONE] remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector);
+  console.log('Title list: ', titleList);
+         
+  function clearTitleLinks(){
+    titleList.innerHTML = '';
+  }
+  clearTitleLinks()
+
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = '';
+
+  for(let article of articles){
+    article.addEventListener('click', generateTitleLinks);
+    console.log(article)
   
+  
+  /* get the article id */
+
+  const articleId = article.getAttribute('id');
+  console.log(articleId)
+
+  /* find the title element */
+
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+  /* get the title from the title element */
+
+  console.log('Title: ',articleTitle);
+
+  /* create HTML of the link */
+
+  const linkHTML = '<li><a href="#'+ articleId +'"><span>'+ articleTitle +'</span></a></li>';
+  console.log(linkHTML);
+
+  /* insert link into titleList */
+  html = html + linkHTML;
+  }
+  
+  titleList.innerHTML = html;
+
   const links = document.querySelectorAll('.titles a');
-  
+  console.log(links);
+
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+    
+generateTitleLinks();
 
+}
